f8586256d6b376a0c9ea5863820ce99ee0dec078
JEFF <jeffreyjohnens@gmail.com>
2018-10-22 23:12:51 -0700

    small change

--- test_sentences.py
+++ test_sentences.py
@@ -54,3 +54,10 @@ def get_readings(sentence):
 	return results
 
 #print get_readings(u"Kyseisen vaatimustenmukaisuutta arvioivien elinten toimintaa voidaan parantaa huomattavasti.")
+
+if __name__ == '__main__':
+	disambiguations = __disambiguate(u"alan laulamaan")
+	for disambiguation in disambiguations:
+		possible_words = disambiguation[1]
+		for possible_word in possible_words:
+			print possible_word.morphology
