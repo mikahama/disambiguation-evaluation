69fc0706fcb10875951d5b548f146365fb6ee395
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2018-10-23 17:21:02 +0300

    myv

--- test_sentences.py
+++ test_sentences.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
 	print get_readings(s)
 	"""
 	UD_PATH = "ud/myv-ud.conllu"
-	fw_map, bw_map = UD_tree_to_mapping(UD_PATH, cache="test_myv.npz")
+	fw_map, bw_map = UD_trees_to_mapping(UD_PATH, cache="test_myv.npz")
 	#produce_tests()
 	dict_to_json("bw_map_myv.json", bw_map)
 	dict_to_json("fw_map_myv.json", fw_map)
