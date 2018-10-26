4a277d7da1c7a68f1feecee2c7392f0b9d8b0929
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2018-10-26 13:20:32 +0300

    tests

--- tester.py
+++ tester.py
@@ -2,14 +2,15 @@ import itertools
 from subprocess import call
 
 train_langs = ["fin", "sme"]
-test_langs = ["myv"]
+test_langs = ["fin", "sme", "kpv", "myv"]
 min_support = ["5", "10", "20"]
 max_pattern = ["5", "20"]
+max_gaps = ["1", "20"]
 
 for args in itertools.product(train_langs, test_langs, min_support, max_pattern):
 	print args
 	try:
-		call(["python", "common.py", args[0], args[1], "--min-sup", args[2], "--max-pattern-length", args[3], "--save-results", "true"])
+		call(["python", "common.py", args[0], args[1], "--min-sup", args[2], "--max-pattern-length", args[3], "--save-results", "true", "--scoring-method", "dep", "--max-gap", args[4]])
 		print "ok"
 	except:
 		print "fail"
